83f7e0346191c293a432b2a89fd2981c21ccb18e
p.mihir@live.com <p.mihir@live.com>
2020-10-31 21:43:45 -0400

    context and refactor

--- src/App.js
+++ src/App.js
@@ -1,114 +1,41 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import About from "./components/pages/About";
-import axios from "axios";
+import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound"
 
-class App extends Component {
-  state = {
-    users: [],
-    user: [],
-    repos: [],
-    loading: false,
-    alert: null,
-  };
+const App = () => {
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ users: res.data, loading: false });
-  }
-
-  // search github users
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ users: res.data.items, loading: false });
-  };
-
-  // get a single github user
-  getUser = async (username) => {
-    this.setState({ loading: true });
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ user: res.data, loading: false });
-  };
-
-  //get user repos
-  getUserRepos = async (username) => {
-    this.setState({ loading: true });
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ repos: res.data, loading: false });
-  };
-
-  clearUsers = () => {
-    this.setState({ users: [], loading: false });
-  };
-
-  setAlert = (message, type) => {
-    this.setState({ alert: { msg: message, type: type } });
-
-    setTimeout(() => this.setState({ alert: null }), 5000);
-  };
-
-  render() {
-    const { users, user, repos, loading } = this.state;
-
+  
     return (
-      <Router>
+
+      <GithubState>
+        <AlertState>
+  <Router>
         <div className='App'>
           <Navbar title='Github Finder' icon='fab fa-github' />
           <div className='container'>
-            <Alert alert={this.state.alert}></Alert>
+            <Alert></Alert>
             <Switch>
-              <Route
-                exact
-                path='/'
-                render={(props) => (
-                  <Fragment>
-                    <Search
-                      searchUsers={this.searchUsers}
-                      clearUsers={this.clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={this.setAlert}></Search>
-                    <Users loading={loading} users={this.state.users}></Users>
-                  </Fragment>
-                )}
-              />
+              <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About}></Route>
-              <Route
-                exact
-                path='/user/:login'
-                render={(props) => (
-                  <User
-                    {...props}
-                    getUser={this.getUser}
-                    getUserRepos={this.getUserRepos}
-                    user={user}
-                    repos={repos}
-                    loading={loading}></User>
-                )}></Route>
+              <Route exact path='/user/:login' component={User}></Route>
+              <Route component={NotFound}></Route>
             </Switch>
           </div>
         </div>
       </Router>
+      </AlertState>
+      </GithubState>
+      
     );
-  }
+    
 }
 
 export default App;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,24 +1,21 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, Fragment, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
+const User = ({match}) => {
 
-  static propTypes = {
-    loading: PropTypes.bool.isRequired,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-  };
+    const githubContext = useContext(GithubContext);
+    const {getUser, user, loading, getUserRepos, repos} = githubContext;
 
-  render() {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    //eslint-dsable-next-line
+  }, [])
+
+
     const {
       name,
       company,
@@ -33,9 +30,8 @@ export class User extends Component {
       public_repos,
       public_gists,
       hireable,
-    } = this.props.user;
+    } = user;
 
-    const { loading, repos } = this.props;
 
     if (loading) return <Spinner></Spinner>;
     return (
@@ -106,7 +102,7 @@ export class User extends Component {
         <Repos repos={repos}></Repos>
       </Fragment>
     );
-  }
 }
+
 
 export default User;
